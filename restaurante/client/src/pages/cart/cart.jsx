17a23f40d4eb08c92aef1c9c43b62b6874b4e05a
Gabriel Revolti <124897816+gabrielrevolti@users.noreply.github.com>
2024-10-30 19:01:36 -0300

    agora o valor do carrinho também aumenta

--- restaurante/client/src/pages/cart/cart.jsx
+++ restaurante/client/src/pages/cart/cart.jsx
@@ -1,42 +1,39 @@
-
-import "./cart.css"
-
+import "./cart.css";
 import { AiOutlineClose } from "react-icons/ai";
 import { useItems } from "../../hooks/useItems";
 import Arrow from "../components/arrow-icon/Arrow";
 
 const Cart = () => {
-  const {cartItems, removeToCart} = useItems()
-  return (
-    <>
-    <div className="cover cart">
+  const { cartItems, removeToCart } = useItems();
 
-      
-    
+  return (
+    <div className="cover cart">
       <div className="container-cart">
-          <h1 className="title-h1">Carrinho</h1>
-          {cartItems.map((item) => (
-            <div className="items-cart" key={item.itemId}>
+        <h1 className="title-h1">Carrinho</h1>
+        {cartItems.map((item) => (
+          <div className="items-cart" key={item.itemId}>
             <div className="first-part">
               <div>
                 <img className="image-cart" src={item.itemImage} alt={item.itemName} />
               </div>
               <div className="information">
-               <p className="title-cart">{item.itemName}</p>
+                <p className="title-cart">{item.itemName}</p>
               </div>
             </div>
-          
             <span className="quantity-cart">{item.itemQuantity}</span>
-            <span className="price-cart">R$ {item.itemPrice}</span>
-            <AiOutlineClose className="x-button" onClick={()=> removeToCart(item.itemId)}/>
+            <span className="price-cart">
+              R$ {(parseFloat(item.itemPrice) * item.itemQuantity).toFixed(2)}
+            </span>
+            <AiOutlineClose 
+              className="x-button" 
+              onClick={() => removeToCart(item.itemId)}
+            />
           </div>
-          ))}
-        </div>
-        <Arrow/>
+        ))}
+      </div>
+      <Arrow />
     </div>
-    </>
-   
-  )
-}
+  );
+};
 
 export default Cart;
